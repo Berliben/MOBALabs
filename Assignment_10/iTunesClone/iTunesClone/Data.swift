93f758639c5979b52739ec1500025c4177307a86
Benjamin <61158773+Berliben@users.noreply.github.com>
2022-05-01 15:51:54 +0200

    updated iTunes Clone

--- Assignment_10/iTunesClone/iTunesClone/Data.swift
+++ Assignment_10/iTunesClone/iTunesClone/Data.swift
@@ -14,6 +14,7 @@ struct Result: Decodable {
 struct Song: Decodable, Identifiable {
     var artistName: String
     var collectionName: String
+    var artworkUrl100: String
     var id: String {
         get {
             return artistName
@@ -22,8 +23,16 @@ struct Song: Decodable, Identifiable {
 }
 
 class Api {
-    func getSongs(completion: @escaping ([Song]) -> ()) {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=The+Rolling+Stones") else { return }
+    func getSongs(searchQuery: String, completion: @escaping ([Song]) -> ()) {
+        
+        var checkedSearchQuery = searchQuery
+        
+        if let range = checkedSearchQuery.range(of: " ") {
+            let replacedString = checkedSearchQuery.replacingCharacters(in: range,with: "+")
+            checkedSearchQuery = replacedString
+        }
+        
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(checkedSearchQuery)&entity=album") else { return }
         
         URLSession.shared.dataTask(with: url) { data, _, _ in
             let songs = try! JSONDecoder().decode(Result.self, from: data!)
--- Assignment_10/iTunesClone/iTunesClone/SongList.swift
+++ Assignment_10/iTunesClone/iTunesClone/SongList.swift
@@ -10,17 +10,34 @@ import SwiftUI
 struct SongList: View {
     
     @State var songs: [Song] = []
+    @State var searchQuery: String = ""
     
     var body: some View {
-        
-        List(songs) { song in
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Artist: \(song.artistName)")
-                Text("Album: \(song.collectionName)")
-            }
-        }.onAppear {
-            Api().getSongs { (songs) in
-                self.songs = songs
+        VStack(spacing: 0) {
+            
+            VStack(spacing: 0) {
+            Text("Fake iTunes").font(.system(size: 25, weight: .bold, design: .default)).padding(.bottom)
+            TextField("Search for album", text: $searchQuery)
+                .padding(.all)
+                .border(Color(UIColor.separator))
+                .padding(.leading)
+                .padding(.trailing)
+                .onSubmit {
+                    Api().getSongs(searchQuery: searchQuery, completion: { songs in
+                        self.songs = songs
+                    })
+                }
+                
+            }.padding(.all)
+            
+            List(songs) { song in
+                HStack(spacing: 40) {
+                    AsyncImage(url: URL(string: song.artworkUrl100))
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Artist: \(song.artistName)")
+                        Text("Album: \(song.collectionName)")
+                    }
+                }
             }
         }
     }
